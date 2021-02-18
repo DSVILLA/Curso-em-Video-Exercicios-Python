afbf66c35cf45f2b9a4caef644ff84a12bd0fc39
Marcos Sousa <equipevilla@live.com>
2021-02-18 16:01:55 -0300

    Create 017 – Quebrando um número.py

---
+++ 017 – Quebrando um número.py
@@ -0,0 +1,9 @@
+from math import trunc
+
+num = float(input('Digite um número: '))
+print('O número digitado foi {} e sua porção inteira é: {}'.format(num, trunc(num)))
+
+#Exemplo 2 sem import
+
+num = float(input('Digite um valor:'))
+print('O número digitado foi {} e sua porção inteira é: {}'.format(num, int(num))
