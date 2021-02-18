8b2ab1473a2cc7044a9fb7717f872f01eb4fd76a
Marcos Sousa <equipevilla@live.com>
2021-02-18 11:49:40 -0300

    Update 013 - Reajuste Salarial.py

--- 013 - Reajuste Salarial.py
+++ 013 - Reajuste Salarial.py
@@ -1,1 +1,6 @@
-s
+salario = float(input('Digite seu salário e veja seu novo salário com 15% de aumento: '))
+
+salario_com_aumento = salario * 0.15
+salario_novo = salario + salario_com_aumento
+
+print('Você ganhou um aumento de RS{}, e seu novo salário agora é: RS{}'.format(salario_com_aumento, salario_novo))
