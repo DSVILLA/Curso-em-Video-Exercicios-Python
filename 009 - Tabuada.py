9eecb1b355e944eaae1f228871aa895f219e8898
Marcos Sousa <equipevilla@live.com>
2021-02-18 02:17:45 -0300

    Create 009 - Tabuada.py

---
+++ 009 - Tabuada.py
@@ -0,0 +1,25 @@
+a = int(input('Digite um número, para ver a tabuada do mesmo: '))
+t0 = a*0
+t1 = a*1
+t2 = a*2
+t3 = a*3
+t4 = a*4
+t5 = a*5
+t6 = a*6
+t7 = a*7
+t8 = a*8
+t9 = a*9
+t10 = a*10
+print('O número digitado foi {}\n\n'.format(a))
+print('Tabuada do {}'.format(a))
+print(' {} x 0 = {} \n'.format(a, t0))
+print(' {} x 1 = {} \n'.format(a, t1))
+print(' {} x 2 = {} \n'.format(a, t2))
+print(' {} x 3 = {} \n'.format(a, t3))
+print(' {} x 4 = {} \n'.format(a, t4))
+print(' {} x 5 = {} \n'.format(a, t5))
+print(' {} x 6 = {} \n'.format(a, t6))
+print(' {} x 7 = {} \n'.format(a, t7))
+print(' {} x 8 = {} \n'.format(a, t8))
+print(' {} x 9 = {} \n'.format(a, t9))
+print(' {} x 10 = {} \n'.format(a, t10))
