c961b9887bdf6fb14c1c7b99b21e7445e14dd6c6
Marcos Sousa <equipevilla@live.com>
2021-02-18 01:31:55 -0300

    Create 006 - Dobro, Triplo, Raiz Quadrada.py

---
+++ 006 - Dobro, Triplo, Raiz Quadrada.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um valor: '))
+d = n1*2
+t = n1*3
+r = n1**(1/2)
+print('O número {} tem 3 fatores: '.format(n1))
+print('O dobro deste numero é: {}'.format(d))
+print('O triplo deste numero é: {}'.format(t))
+print('E a raiz quadrada deste numero é: {}'.format(r))
