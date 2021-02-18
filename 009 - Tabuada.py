94c1ef659fb1ab31a17989611bb54d99d3f56668
Marcos Sousa <equipevilla@live.com>
2021-02-18 02:49:01 -0300

    Update 009 - Tabuada.py

--- 009 - Tabuada.py
+++ 009 - Tabuada.py
@@ -23,3 +23,22 @@ print(' {} x 7 = {} \n'.format(a, t7))
 print(' {} x 8 = {} \n'.format(a, t8))
 print(' {} x 9 = {} \n'.format(a, t9))
 print(' {} x 10 = {} \n'.format(a, t10))
+
+
+
+# Exemplo professor
+
+num = int(input('Digite um número, para ver a tabuada do mesmo: '))
+print('============== TABUADA ==============')
+print('{} X {} = {}'.format(num, 0, num*0))
+print('{} X {} = {}'.format(num, 1, num*1))
+print('{} X {} = {}'.format(num, 2, num*2))
+print('{} X {} = {}'.format(num, 3, num*3))
+print('{} X {} = {}'.format(num, 4, num*4))
+print('{} X {} = {}'.format(num, 5, num*5))
+print('{} X {} = {}'.format(num, 6, num*6))
+print('{} X {} = {}'.format(num, 7, num*7))
+print('{} X {} = {}'.format(num, 8, num*8))
+print('{} X {} = {}'.format(num, 9, num*9))
+print('{} X {} = {}'.format(num, 10, num*10))
+print('=====================================')
