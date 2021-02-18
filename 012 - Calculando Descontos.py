b2096c5437c0ef85462496df6b5f357d82a7ddaa
Marcos Sousa <equipevilla@live.com>
2021-02-18 11:46:48 -0300

    Update 012 - Calculando Descontos.py

--- 012 - Calculando Descontos.py
+++ 012 - Calculando Descontos.py
@@ -1,5 +1,5 @@
 produto = float(input('Digite o preço do produto para calcular o novo valor com 5% de desconto: '))
 
-desconto = produto * 0.5 /10
+desconto = produto * 0.5 / 10
 valor_com_desconto = produto * 0.95
 print('Você ganhou R${} em descontos, o novo valor com desconto agora é: R${:.3f}'.format(desconto, valor_com_desconto))
