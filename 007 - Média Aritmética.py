d17c280c55a2bb320fd17e07e692349217028ad9
Marcos Sousa <equipevilla@live.com>
2021-02-18 02:41:27 -0300

    Update 007 - Média Aritmética.py

--- 007 - Média Aritmética.py
+++ 007 - Média Aritmética.py
@@ -1,7 +1,7 @@
 print('========= Calculadora de Média Aluno =========')
-n1 = int(input('Digite sua N1: '))
-n2 = int(input('Digite sua N2: '))
+n1 = float(input('Digite sua N1: '))
+n2 = float(input('Digite sua N2: '))
 
-media = n1 + n2 / 2
+media = (n1 + n2) / 2
 
 print('Sua média é: {}'.format(media))
