62742c618cfe94c24240412a6cda669a84e1ea38
Marcos Sousa <equipevilla@live.com>
2021-02-18 01:33:22 -0300

    Create 007 - Média Aritmética.py

---
+++ 007 - Média Aritmética.py
@@ -0,0 +1,7 @@
+print('========= Calculadora de Média Aluno =========')
+n1 = int(input('Digite sua N1: '))
+n2 = int(input('Digite sua N2: '))
+
+media = n1 + n2 / 2
+
+print('Sua média é: {}'.format(media))
