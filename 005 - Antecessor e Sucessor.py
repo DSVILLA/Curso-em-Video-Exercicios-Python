685789f9ebb41a9d3ca831cca660e617663ad08f
Marcos Sousa <equipevilla@live.com>
2021-02-18 01:30:15 -0300

    Create 005 - Antecessor e Sucessor.py

---
+++ 005 - Antecessor e Sucessor.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um número e veja a magica: '))
+
+a = n1-1
+b = n1+1
+
+print('O numero digitado é {}'.format(n1))
+print('O antecessor é: {}'.format(a))
+print('O sucessor é: {}'.format(b))
