deed1cda46dd98a394a3a1530a2e7be364dc5e25
Marcos Sousa <equipevilla@live.com>
2021-02-18 01:50:54 -0300

    Create 002 - Respondendo ao Usuário.py

---
+++ 002 - Respondendo ao Usuário.py
@@ -0,0 +1,4 @@
+nome = str(input('Qual seu nome? '))
+idade = int(input('Qual a sua idade? '))
+local = str(input('Em qual país você mora? '))
+print('Olá {}, prazer em conhece-lo, agora sei que você tem {} anos de idade e mora em {}'.format(nome, idade, local))
