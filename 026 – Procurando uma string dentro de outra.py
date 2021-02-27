3ffc196d1266a27415cada4d1473f3a734fe96fa
Marcos Sousa <equipevilla@live.com>
2021-02-27 00:30:21 -0300

    Create 026 – Procurando uma string dentro de outra.py

---
+++ 026 – Procurando uma string dentro de outra.py
@@ -0,0 +1,5 @@
+#exercicio para verificar se tem silva no nome, idepedente de sua posição.
+
+nome = str(input('Digite seu nome completo: '))
+formatacao = nome.upper()
+print('SILVA' in formatacao)
