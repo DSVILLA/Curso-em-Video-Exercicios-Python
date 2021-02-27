ee0527f59d90cacce14f2910b2d55e1ae78e23eb
Marcos Sousa <equipevilla@live.com>
2021-02-27 00:45:35 -0300

    Create 033 – Ano Bissexto.py

---
+++ 033 – Ano Bissexto.py
@@ -0,0 +1,8 @@
+print(' --- Descubra se o ano é bissexto --- ')
+
+ano = int(input('Digite o ano: '))
+if (ano%4==0 and ano%100!=0) or (ano%400==0):
+    print('É um ano bissexto!')
+
+else:
+    print('Não é um ano bissexto!')
