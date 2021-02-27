758a5c5f06f4bed6b3e7688ca2db2098ad20afa3
Marcos Sousa <equipevilla@live.com>
2021-02-27 01:02:12 -0300

    Update 035 – Aumentos múltiplos.py

--- 035 – Aumentos múltiplos.py
+++ 035 – Aumentos múltiplos.py
@@ -1,8 +1,6 @@
 sal = float(input('Digite seu salário: '))
-if sal > 1.250:
-    sal_10 = sal * 0.10
-    print('Seu novo salário é de R${:.1f}'.format(sal_10+sal))
-
+if sal <= 1.250:
+    sal_novo = sal + ( sal * 15 / 100)
 else:
-    sal_15 = sal * 0.15
-    print('Seu novo salário é de R${:.1f}'.format(sal_15+sal))
+    sal_novo = sal + ( sal * 15 / 100)
+    print('Seu novo salário é de R${:.1f}'.format(sal_novo))
